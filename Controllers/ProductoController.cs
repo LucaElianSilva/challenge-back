7fb9ad34ea5090aeb98fe857c5634d81b3977603
LSilva <lucas@shifta.la>
2023-01-11 05:22:34 -0300

    Back en proceso(Creacion de DataContext, Migraciones, Entidades, Controllers y Seeds)

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using challenge_back.Entities;
+using challenge_back.Models;
+using Microsoft.AspNetCore.Mvc;
 namespace challenge_back.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class ProductoController : ControllerBase
     {
+        private readonly ChallengeContext _dbContext;
+        public ProductoController(ChallengeContext dbContext) { 
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("GetProductos")]
+        public async Task<IActionResult> GetProductos()
+        {
+
+            try
+            {
+                List<Producto> productos = _dbContext.Productos.ToList();
+                return Ok(productos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
